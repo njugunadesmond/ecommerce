e14d798f65e1bbd770deb2c489f33512b5bdeace
Njuguna Mwangi <51181484+njugunamwangi@users.noreply.github.com>
2021-08-23 14:13:54 +0300

    move nav items to the right

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -50,7 +50,7 @@ AppAsset::register($this);
             . '</li>';
     }
     echo Nav::widget([
-        'options' => ['class' => 'navbar-nav'],
+        'options' => ['class' => 'navbar-nav ml-auto'],
         'items' => $menuItems,
     ]);
     NavBar::end();

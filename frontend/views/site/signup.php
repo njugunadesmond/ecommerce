b403f6664097bc5973cfb1edf7da9ee53a7ad68e
Njuguna Mwangi <51181484+njugunamwangi@users.noreply.github.com>
2021-08-23 13:59:42 +0300

    format signup form

--- frontend/views/site/signup.php
+++ frontend/views/site/signup.php
@@ -11,9 +11,12 @@ $this->title = 'Signup';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-signup">
-    <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>Please fill out the following fields to signup:</p>
+    <div class="row justify-content-center">
+        <div class="col-lg-6">
+            <h1><?= Html::encode($this->title) ?></h1>
+
+            <p>Please fill out the following fields to signup:</p>
 
     <div class="row">
         <div class="col-lg-5">

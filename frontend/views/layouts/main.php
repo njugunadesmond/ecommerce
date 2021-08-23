690583a7861102b1f016804b078a66c1d8ad4da4
Njuguna Mwangi <51181484+njugunamwangi@users.noreply.github.com>
2021-08-23 14:14:21 +0300

    Display first and last name on nav bar

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -44,7 +44,7 @@ AppAsset::register($this);
         $menuItems[] = '<li>'
             . Html::beginForm(['/site/logout'], 'post', ['class' => 'form-inline'])
             . Html::submitButton(
-                'Logout (' . Yii::$app->user->identity->username . ')',
+                'Logout (' . Yii::$app->user->identity->getDisplayName() . ')',
                 ['class' => 'btn btn-link logout']
             )
             . Html::endForm()
